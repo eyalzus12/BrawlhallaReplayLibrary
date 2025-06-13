4d6338ea8d05f6fb4cda7c6aa754db5991580b06
EyalZusiman <eyal.zusiman@gmail.com>
2025-06-13 15:32:39 +0300

    update ReplayGameModeFlags to modern values

--- src/ReplayObjects/ReplayGameModeFlags.cs
+++ src/ReplayObjects/ReplayGameModeFlags.cs
@@ -9,12 +9,10 @@ public enum ReplayGameModeFlags : uint
     Teams = 0b0000000001,
     TeamDamage = 0b0000000010,
     FixedCamera = 0b00000000100,
-    GadgetsOff = 0b00000001000,
-    WeaponsOff = 0b00000010000,
+    AltMode = 0b00000001000,
     TestLevelsOn = 0b00000100000,
     TestFeaturesOn = 0b00001000000,
     GhostRule = 0b00010000000,
     TurnOffMapArtThemes = 0b00100000000,
     ForceCrewBattleCycle = 0b01000000000,
-    AdvancedSettings = 0b10000000000,
 }
